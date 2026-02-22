ac733254a5e4661b0011dbdc455e79784b48da15
Adam Michael Bauer <ambauer@uchicago.edu>
2026-02-21 22:00:36 -0600

    feat: command line parsing

---
+++ src/var_assim/config.py
@@ -0,0 +1,100 @@
+"""Config file that loads paths from YAML.
+
+Adam Bauer
+UChicago
+Jan 2026
+"""
+
+import yaml
+import argparse 
+
+from pathlib import Path
+
+CONFIG_PATH = Path(__file__).parent.parent.parent / "config" / "dirs.yaml"
+
+with open(CONFIG_PATH, "r") as f:
+    CONFIG = yaml.safe_load(f)
+
+DATA_DIR = Path(CONFIG["DATA_DIR"])
+FIGS_DIR = Path(CONFIG["FIGS_DIR"])
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Estimating future climate uncertainty using pseudo-observations and ensemble variational data assimilation."
+    )
+
+    # emissions scenario
+    parser.add_argument(
+        "--scenario",
+        type=str,
+        default="ssp245"
+    )
+
+    # start time of model
+    parser.add_argument(
+        "--tmin",
+        type=int,
+        default=2025
+    )
+
+    # noise model
+    parser.add_argument(
+        "--noise_model",
+        type=str,
+        default="AR1"
+    )
+
+    # true value of angle parameter
+    parser.add_argument(
+        "--theta",
+        type=int,
+        default=14
+    )
+
+    # true value of ECS
+    parser.add_argument(
+        "--ecs",
+        type=float,
+        default=3.0
+    )
+
+    # SAI cooling per decade
+    parser.add_argument(
+        "--dep_p_dec",
+        type=float,
+        default=0.1
+    )
+
+    # number of years ramp up for SAI
+    parser.add_argument(
+        "--n_yrs_ramp",
+        type=int,
+        default=50
+    )
+
+    # number of ensemble members
+    parser.add_argument(
+        "--n_ens",
+        type=int,
+        default=500
+    )
+
+    # manual override of assimilation windows?
+    parser.add_argument(
+        "--override_windows",
+        action='store_true'
+    )
+
+    # add debugging mode
+    parser.add_argument(
+        "--debug",
+        action='store_true'
+    )
+
+    return parser.parse_args()
+
+
+# quick tester of intuition
+if __name__ == "__main__":
+    args = parse_args()
+    print(args)
