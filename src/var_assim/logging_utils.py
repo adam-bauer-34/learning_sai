0aee97138fa7df372b210d16ed5c570d532d9789
Adam Michael Bauer <ambauer@uchicago.edu>
2026-02-23 16:49:01 -0600

    feat: done adding logging utils

--- src/var_assim/logging_utils.py
+++ src/var_assim/logging_utils.py
@@ -25,15 +25,21 @@ def setup_logger(debug=False):
 
     logger = logging.getLogger("var_assim")
 
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)s | %(message)s",
         "%Y-%m-%d %H:%M:%S"
     )
 
     stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(formatter)
     stdout_handler.setLevel(logging.DEBUG if debug else logging.INFO)
     stdout_handler.addFilter(lambda r: r.levelno < logging.WARNING)
+    stdout_handler.setFormatter(formatter)
 
     stderr_handler = logging.StreamHandler(sys.stderr)
     stderr_handler.setLevel(logging.WARNING)
@@ -48,3 +54,5 @@ def setup_logger(debug=False):
 if __name__ == "__main__":
     logger = setup_logger(debug=True)
     logger.info(f"Git: {get_git_hash()}")
+    logger.warning("Yo!")
+    logger.error("Oh no!")
