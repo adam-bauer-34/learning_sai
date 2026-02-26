9078567b11397551318596260713b88a0156f833
Adam Michael Bauer <ambauer@uchicago.edu>
2026-02-26 15:12:51 -0600

    feat: dask setup function detects local vs slurm

--- src/var_assim/dask_setup.py
+++ src/var_assim/dask_setup.py
@@ -8,6 +8,8 @@ Feb 2026
 import os 
 
 from dask.distributed import Client, LocalCluster
+from multiprocessing import cpu_count
+
 
 def start_dask(logger):
     """Start dask config.
@@ -21,20 +23,31 @@ def start_dask(logger):
     client: LocalCluster dask object
     """
 
-    # Slurm sets the SLURM_CPUS_PER_TASK variable
-    # We use this to tell Dask exactly how many workers to start
-    cpus_available = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
-    
-    # Initialize a cluster that matches the Slurm allocation
+    # detect if we're on slurm
+    on_slurm = "SLURM_JOB_ID" in os.environ
+
+    # if we're on slurm, set the number of CPUs based on SLURM config
+    if on_slurm:
+        cpus_available = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
+        logger.info(f"Slurm environment detected. Using {cpus_available} allocated CPUs.")
+
+    # otherwise, we're on local, so use the number of cores minus one
+    else:
+        cpus_available = max(1, cpu_count() - 1)  # leave 1 core free locally
+        logger.info(f"Local environment detected. Using {cpus_available} of {cpu_count()} CPUs.")
+
+    # initialize local dask cluseter
     cluster = LocalCluster(
-        n_workers=cpus_available, 
-        threads_per_worker=1,  # 1 thread per worker is best for heavy math
+        n_workers=cpus_available,
+        threads_per_worker=1,
         memory_limit='auto',
         processes=True
     )
+
+    # setup Client
     client = Client(cluster)
-    
-    # add logs
+
+    # print config details
     logger.info(f"Dask Client started with {cpus_available} workers.")
     logger.info(f"Dashboard link: {client.dashboard_link}")
 
