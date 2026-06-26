f4a1e8e0ea6fd3df95cd7375cf7e6fa4ff9a4ba8
Adam Michael Bauer <ambauer@uchicago.edu>
2026-06-26 15:35:55 -0500

    new reg noise modeL

---
+++ src/var_assim/models/pco2geowc_reg/parallelization.py
@@ -0,0 +1,188 @@
+"""4DVAR Runner.
+
+Adam Michael Bauer
+University of Illinois Urbana-Champaign
+8.6.2024
+"""
+
+import numpy as np
+
+from .cost import cost, grad
+from .dynamics import get_nonlin_path
+from scipy.optimize import minimize
+
+
+class EnsembleMember:
+    """4DVAR Ensemble Member Class."""
+
+    def __init__(
+        self,
+        theta_p,
+        flag,
+        tol,
+        max_iter,
+        TMIN,
+        TMAX,
+        DT,
+        theta_tr,
+        inv_covar_prior,
+        inv_covar_T1_obs,
+        inv_covar_Q_obs,
+        inv_covar_T_R1_obs,
+        inv_covar_T_R2_obs,
+        obs,
+        times,
+    ):
+
+        # set class attributes
+        self.theta_p = theta_p
+        self.flag = flag
+        self.tol = tol
+        self.max_iter = max_iter
+        self.TMIN = TMIN
+        self.TMAX = TMAX
+        self.DT = DT
+        self.theta_tr = theta_tr
+        self.inv_covar_prior = inv_covar_prior
+        self.inv_covar_T1_obs = inv_covar_T1_obs
+        self.inv_covar_Q_obs = inv_covar_Q_obs
+        self.inv_covar_T_R1_obs = inv_covar_T_R1_obs
+        self.inv_covar_T_R2_obs = inv_covar_T_R2_obs
+        self.obs = obs
+
+        # make histories for cost, l2, time series, and controls
+        self.data_hist = np.zeros(
+            (len(theta_p), max_iter + 1, len(times)), dtype=np.float32
+        )
+        self.controls_hist = np.zeros((len(theta_p), max_iter + 1), dtype=np.float32)
+        self.cost_hist = np.zeros(max_iter + 1, dtype=np.float32)
+        self.l2s_hist = np.zeros(max_iter + 1, dtype=np.float32)
+
+        # set prior in controls history
+        self.controls_hist[:, 0] = theta_p
+
+
+def runner_4dvar(mem, e):
+    """4DVAR "runner" function.
+
+    This function is designed to be used in tandem with Dask to parallelize the
+    4DVAR procedure.
+
+    Parameters
+    ----------
+    mem: `EnsembleMember` class
+        ensemble member class
+
+    e: `EmissionsBaseline` class
+        contains emissions information that is shared among Dask workers
+
+    Returns
+    -------
+    mem: `EnsembleMember` class
+        ensemble member class with additional attributes related to 4DVAR
+        simulation
+    """
+
+    # set the initial value of the l2 norm and store
+    mem.l2 = np.sqrt(np.sum((np.array(mem.theta_p) - mem.theta_tr) ** 2))
+    mem.l2s_hist[0] = mem.l2
+
+    # set the iteration counter
+    iter_ = 1
+
+    # get prior paths
+    prior_p, _ = get_nonlin_path(e, mem.theta_p, mem.TMIN, mem.TMAX, mem.DT)
+
+    # set prior paths as first entry in data history
+    mem.data_hist[:, 0] = prior_p
+
+    # compute the cost function for prior and store in history
+    J0 = cost(
+        mem.theta_p,
+        args=[
+            mem.theta_p,
+            mem.inv_covar_prior,
+            mem.inv_covar_T1_obs,
+            mem.inv_covar_Q_obs,
+            mem.inv_covar_T_R1_obs,
+            mem.inv_covar_T_R2_obs,
+            mem.obs,
+            e,
+            mem.TMIN,
+            mem.TMAX,
+            mem.DT,
+        ],
+    )
+
+    mem.cost_hist[0] = J0
+
+    # set the current member control as the prior
+    mem.control = mem.theta_p
+
+    # while the l2 norm is higher than the tolerance, do the following 4DVAR
+    # process.
+    while mem.l2 > mem.tol:
+        # solve optimization problem
+        bounds = np.array([(-np.inf, np.inf) for cont in mem.control])
+        bounds[5:13, 0] = 0  # L, G, EPS, C1, C2, F1, a1, a2 >= 0
+        bounds[15:, :] = (
+            -1.2,
+            1.2,
+        )  # implicit bound on global model errors of 4.5 sigma and regional ~3
+
+        sol = minimize(
+            cost,
+            x0=mem.control,
+            args=[
+                mem.control,
+                mem.inv_covar_prior,
+                mem.inv_covar_T1_obs,
+                mem.inv_covar_Q_obs,
+                mem.inv_covar_T_R1_obs,
+                mem.inv_covar_T_R2_obs,
+                mem.obs,
+                e,
+                mem.TMIN,
+                mem.TMAX,
+                mem.DT,
+            ],
+            bounds=bounds,
+            method="SLSQP",
+            jac=grad,
+        )
+
+        # print(sol.fun)
+
+        # set optimal solution as new estimate of control variables
+        new_theta = sol.x
+
+        # store the optimal cost function in the history
+        mem.cost_hist[iter_] = sol.fun
+
+        # compute L2 norm between truth and optimal value and store
+        mem.l2 = np.sqrt(np.sum((new_theta - mem.theta_tr) ** 2))
+        mem.l2s_hist[iter_] = mem.l2
+
+        # store new trajectory in the data history
+        new_p, _ = get_nonlin_path(e, new_theta, mem.TMIN, mem.TMAX, mem.DT)
+        mem.data_hist[:, iter_] = new_p
+
+        # if diff > tol, set the first guess as the optimal solution and
+        # try again
+        mem.control = new_theta
+        mem.controls_hist[:, iter_] = mem.control
+
+        # index the iteration counter
+        iter_ += 1
+
+        if iter_ > mem.max_iter:
+            # we've exceeded maximum iterations, and we end the 4DVAR
+            # process
+            mem.flag = 1
+            break
+
+    # set final values
+    mem.data = new_p
+    mem.cost = sol.fun
+
+    return mem
---
+++ src/var_assim/models/pco2geowc_reg/runner.py
@@ -0,0 +1,359 @@
+"""Two layer model 4DVAR with internal variability.
+
+Adam Michael Bauer
+University of Illinois Urbana Champaign
+8.23.2024
+"""
+
+import os
+import sys
+import time
+import warnings
+import logging
+import argparse
+
+# filter out runtime warnings which clog log files
+# (they are natural in the scipy.minimize call)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+import numpy as np
+from dask.distributed import performance_report
+
+from var_assim.dask import start_dask, run_ensemble
+from var_assim.warm_start import warm_start_simulation
+from var_assim.emis import EmissionsBaseline
+from var_assim.model_errors import gen_noise_ts
+from var_assim.tlm_adj_checks import run_component_checks
+from var_assim.stats.covar import get_covar_white
+from var_assim.stats.draws import get_prior_draws
+from var_assim.postprocessing import process_simulation_window, make_master_datatree
+from var_assim.config import (
+    opt_config,
+    DATA_DIR,
+    PERF_REPS_PATH,
+    PRIOR_SEED,
+    MOD_ERROR_SEED,
+    REG1_NOISE_SEED,
+    REG2_NOISE_SEED,
+)
+
+from var_assim.models.pco2geowc.dynamics import get_nonlin_path
+from var_assim.models.pco2geowc.obs import get_obs_from_dynamics
+from var_assim.models.pco2geowc.parallelization import EnsembleMember, runner_4dvar
+
+SLURM_JOB_ID = os.environ.get("SLURM_JOB_ID", "local")
+
+
+def run_var_assim_experiment(
+    logger: logging.Logger,
+    args: argparse.Namespace,
+    Prior: object,
+    Truth: object,
+    Noise: object,
+    Windowing: object,
+):
+    # start dask
+    c = start_dask(logger)
+    logger.info(f"    > {c}")
+
+    # set time discretization (always 1.0)
+    DT = 1.0
+
+    """WARM START MODULE.
+    """
+    logger.info("    > Starting warm start module")
+    warm_start_simulation(logger, args, Truth, Prior, get_nonlin_path)
+    logger.info("    > Warm start complete")
+
+    """ASSIMILATION MODULE
+    """
+    # dictionary to make datatree out of later
+    results_dict = {}
+
+    for TMIN, TMAX in Windowing.windows:
+        logger.info(f"    > Carrying out data assimilation for window {TMIN}-{TMAX}")
+
+        # make emissions baseline
+        e = EmissionsBaseline(
+            logger,
+            args,
+            TMIN,
+            TMAX,
+            geo=True,
+            Prior=Prior,
+            Truth=Truth,
+            T_START=TMIN,
+            T_END=TMIN + args.n_yrs_ramp,
+            print_level=2,
+        )
+
+        N_timesteps = len(e.conc["CO2"])
+
+        # make global model errors and their covariance matrix
+        mod_errors_rng = np.random.default_rng(seed=MOD_ERROR_SEED)
+        mod_errors, mod_error_covar = gen_noise_ts(
+            Noise, N_timesteps, rng=mod_errors_rng
+        )
+
+        # make regional covariance matrices
+        r1_rng = np.random.default_rng(seed=REG1_NOISE_SEED)
+        r2_rng = np.random.default_rng(seed=REG2_NOISE_SEED)
+
+        # regions (in this case, 2)
+        R1_mod_error_covar, R2_mod_error_covar = [
+            get_covar_white(
+                np.array([INT_T_REGx_STD] * N_timesteps),
+                N_timesteps,
+                inv=True,
+            )
+            for INT_T_REGx_STD in Noise.INT_T_REG_STD
+        ]
+
+        # make model error time series
+        mod_errors_r1 = r1_rng.multivariate_normal(
+            np.array([0.0] * N_timesteps), R1_mod_error_covar
+        )
+
+        mod_errors_r2 = r2_rng.multivariate_normal(
+            np.array([0.0] * N_timesteps), R2_mod_error_covar
+        )
+
+        logger.debug(f"    ! region 1 model errors: {mod_errors_r1}")
+        logger.debug(f"    ! region 2 model errors: {mod_errors_r2}")
+
+        # combine all model errors into one long vector
+        all_mod_errors = np.hstack([mod_errors, mod_errors_r1, mod_errors_r2])
+
+        # true vector of controls for this window
+        controls_tr = Truth.get_augmented_truth_vector(all_mod_errors)
+
+        if args.debug:
+            logger.info(f"        >> (DEBUG) Controls vector: {controls_tr}")
+            logger.info(
+                f"        >> (DEBUG) Controls vector length: {len(controls_tr)}"
+            )
+
+        # central value of priors on each parameter
+        controls_cen = Prior.get_augmented_cen_vector(np.zeros_like(all_mod_errors))
+
+        # make true data path over this time window
+        data_tr_p, times = get_nonlin_path(e, controls_tr, TMIN, TMAX, DT=1.0)
+
+        # make prior stds vector
+        regional_stds = np.hstack(
+            [[INT_T_REGx_STD] * N_timesteps for INT_T_REGx_STD in Noise.INT_T_REG_STD]
+        )
+        # NOTE: mod errors can be red, so insert dummy here an insert their inverse cov
+        # matrix later
+        full_std_vector = np.hstack([np.ones_like(mod_errors), regional_stds])
+
+        # make full stds vector
+        prior_stds = Prior.get_augmented_std_vector(full_std_vector)
+        logger.debug(f"    ! full std vector for prior: {prior_stds}")
+
+        # make inverse covariance matrices for white noise
+        inv_covar_prior = get_covar_white(prior_stds, len(prior_stds), inv=True)
+
+        # add in inverse covarianace matrix of model errors (which may not be
+        # white, like the other parameters)
+        inv_covar_prior[15 : 15 + len(mod_errors), 15 : 15 + len(mod_errors)] = (
+            np.linalg.inv(mod_error_covar)
+        )
+        logger.debug(f"    ! inverse covariance matrix for prior: {inv_covar_prior}")
+
+        # make observation error covariance matrices
+        # global temp
+        inv_covar_T1_obs = get_covar_white(
+            np.array([Noise.OBS_T1_STD] * len(times)), len(times), inv=True
+        )
+
+        # ocean heat content
+        inv_covar_Q_obs = get_covar_white(
+            np.array([Noise.OBS_Q_STD] * len(times)), len(times), inv=True
+        )
+
+        # regions (in this case, 2)
+        inv_covar_T_R1_obs, inv_covar_T_R2_obs = [
+            get_covar_white(
+                np.array([OBS_T_REGx_STD] * len(times)), len(times), inv=True
+            )
+            for OBS_T_REGx_STD in Noise.OBS_T_REG_STD
+        ]
+
+        # make observations from true data without any additional noise
+        obs = get_obs_from_dynamics(data_tr_p, noise=False)
+
+        # If flagged, check components of variational data assimilation module
+        if args.check_components and TMAX == 2100:
+            logger.info(
+                "        >> (FLAGGED) Checking TLM, ADJ, and cost function gradient accuracy"
+            )
+            run_component_checks(
+                args,
+                e,
+                controls_tr,
+                TMIN,
+                TMAX,
+                cost_args=[
+                    controls_tr,
+                    inv_covar_prior,
+                    inv_covar_T1_obs,
+                    inv_covar_Q_obs,
+                    inv_covar_T_R1_obs,
+                    inv_covar_T_R2_obs,
+                    obs,
+                    e,
+                    TMIN,
+                    TMAX,
+                    DT,
+                ],
+            )
+            logger.info(f"        >> (FLAGGED) Checks complete")
+            logger.info(
+                f"        >> (FLAGGED) .csv files saved to {DATA_DIR}/checks/{args.model}"
+            )
+
+        # optimization parameters
+        tol = opt_config["tol"]
+        max_iter = opt_config["max_iter"]
+
+        # give first guess at initial conditions
+        prior_rng = np.random.default_rng(seed=PRIOR_SEED)
+        theta_prior = get_prior_draws(
+            args.model,
+            controls_cen,
+            np.linalg.inv(inv_covar_prior),
+            args.n_ens,
+            rng=prior_rng,
+        )
+
+        logger.debug(
+            f"    ! mean of parameter prior: {np.mean(theta_prior[:, 15], axis=0)}"
+        )
+        logger.debug(
+            f"    ! median of parameter prior: {np.median(theta_prior[:, 15], axis=0)}"
+        )
+        logger.debug(
+            f"    ! std of parameter prior: {np.std(theta_prior[:, 15], axis=0)}"
+        )
+
+        # Check on object sizes
+        if args.debug:
+            logger.info(
+                f"        >> (DEBUG) Emissions object is of size: {sys.getsizeof(e) / 1e6} MB"
+            )
+
+        # scatter emissions baseline class and true observations to each
+        # dask worker
+        e_scat = c.scatter(e, broadcast=True)
+
+        # make list of ensemble members
+        ensemble_members = [
+            EnsembleMember(
+                theta_p,
+                -1,
+                tol,
+                max_iter,
+                TMIN,
+                TMAX,
+                DT,
+                controls_tr,
+                inv_covar_prior,
+                inv_covar_T1_obs,
+                inv_covar_Q_obs,
+                inv_covar_T_R1_obs,
+                inv_covar_T_R2_obs,
+                obs,
+                times,
+            )
+            for theta_p in theta_prior
+        ]
+
+        if args.debug:
+            m = ensemble_members[0]
+
+            total = 0
+            for v in vars(m).values():
+                if hasattr(v, "nbytes"):
+                    total += v.nbytes
+                else:
+                    total += sys.getsizeof(v)
+
+            logger.debug(
+                f"        >> (DEBUG) Estimated total of on ensemble member: {total / 1e6} MB"
+            )
+            logger.debug(
+                f"        >> (DEBUG) Memory overhead for entire ensemble: {total * args.n_ens / 1e6} MB"
+            )
+
+        # solve the assimilation using dask
+        t0_assim = time.time()
+
+        # do dask evaluation of runner
+        logger.info(
+            f"        >> (TIME INTENSIVE) Carrying out inner and outer loops of variational data assimilation"
+        )
+
+        with performance_report(
+            filename=f"{PERF_REPS_PATH}/perf_report_{SLURM_JOB_ID}.html"
+        ):
+            opt_ensmems = run_ensemble(c, ensemble_members, e_scat, args, runner_4dvar)
+
+        t1_assim = time.time()
+
+        RUNTIME = t1_assim - t0_assim
+        logger.info(f"        >> Ensemble data assimilation solved in {RUNTIME} s")
+
+        logger.info(f"        >> Processing simulation output")
+
+        # cancel scattered emissions
+        c.cancel(e_scat)
+
+        # make variable names list for saving
+        var_names = np.hstack(
+            [
+                [
+                    "T1",
+                    "T2",
+                    "Q",
+                    "T_R1",
+                    "T_R2",
+                    "L",
+                    "G",
+                    "EPS",
+                    "C1",
+                    "C2",
+                    "F1_CO2",
+                    "ALPHA_R1",
+                    "ALPHA_R2",
+                    "BETA_R1",
+                    "BETA_R2",
+                ],
+                ["qAT_" + str(i) for i in range(len(times))],
+                ["qR1_" + str(i) for i in range(len(times))],
+                ["qR2_" + str(i) for i in range(len(times))],
+            ]
+        )
+
+        obs_names = ["T1", "Q", "T_R1", "T_R2"]
+
+        # process simulation output into
+        ds = process_simulation_window(
+            args,
+            var_names,
+            obs_names,
+            TMAX,
+            opt_ensmems,
+            obs,
+            data_tr_p,
+            controls_tr,
+            opt_config,
+            RUNTIME,
+        )
+
+        results_dict[str(TMAX)] = ds
+
+        logger.info(f"        >> Process for window {TMIN}-{TMAX} complete")
+
+    # synthesize datasets from each window into a datatree object
+    make_master_datatree(logger, args, results_dict)
